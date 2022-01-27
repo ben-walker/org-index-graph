890b9fe6530820e1c29f0d72a8aa0ad18710108a
Ben Walker <b6nw4r@gmail.com>
2022-01-26 22:17:02 -0500

    Add node env variables

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,4 +1,6 @@
 const { env } = process;
 
-export const CORS_ORIGIN = env.CORS_ORIGIN?.split(",") || true;
+export const NODE_ENV = env.NODE_ENV || "development";
+export const IS_PRODUCTION = NODE_ENV === "production";
 export const PORT = env.PORT || 4_000;
+export const CORS_ORIGIN = env.CORS_ORIGIN?.split(",") || true;
