bfe5f66165190348e31240565efaeea76e4d5677
Ben Walker <b6nw4r@gmail.com>
2022-01-26 22:41:24 -0500

    Convert port to number

--- src/env/index.ts
+++ src/env/index.ts
@@ -2,5 +2,5 @@ const { env } = process;
 
 export const NODE_ENV = env.NODE_ENV || "development";
 export const IS_PRODUCTION = NODE_ENV === "production";
-export const PORT = env.PORT || 4_000;
+export const PORT = Number.parseInt(env.PORT || "", 10) || 4_000;
 export const CORS_ORIGIN = env.CORS_ORIGIN?.split(",") || true;
