3bbb2ea0805062eb3c31da97ab2e2d14d4193d88
Ben Walker <b6nw4r@gmail.com>
2022-03-19 17:21:26 -0400

    Correctly type server context function

--- src/server/context.ts
+++ src/server/context.ts
@@ -1,5 +1,11 @@
+import { ContextFunction } from "apollo-server-core";
+import { ExpressContext } from "apollo-server-express";
+
 import { prisma } from "../prisma";
 import { Context } from "./types";
 
 // TODO: add user to context via access token in request
-export const getServerContext = (): Context => ({ prisma });
+export const getServerContext: ContextFunction<
+  ExpressContext,
+  Context
+> = () => ({ prisma });
