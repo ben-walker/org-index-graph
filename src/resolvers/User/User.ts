de1b7edd26740a90055e7766f82b4026aefd9f07
Ben Walker <b6nw4r@gmail.com>
2022-03-19 12:26:32 -0400

    Add signUp mutation

--- src/resolvers/User/User.ts
+++ src/resolvers/User/User.ts
@@ -4,8 +4,57 @@ import {
   FindManyUserResolver,
   FindUniqueUserResolver,
   GroupByUserResolver,
+  User,
   UserRelationsResolver,
 } from "@generated/type-graphql";
+import { argon2id, hash } from "argon2";
+import { IsEmail } from "class-validator";
+import {
+  Arg,
+  Ctx,
+  Field,
+  InputType,
+  Mutation,
+  ObjectType,
+  Resolver,
+} from "type-graphql";
+
+import { Context } from "../../server";
+
+@ObjectType()
+class AuthPayload {
+  @Field()
+  user!: User;
+}
+
+@InputType()
+class SignUpInput implements Partial<User> {
+  @Field()
+  @IsEmail()
+  email!: string;
+
+  @Field()
+  name!: string;
+
+  @Field()
+  password!: string;
+}
+
+@Resolver()
+class UserAuthResolver {
+  @Mutation(() => AuthPayload)
+  async signUp(
+    @Arg("userData") { email, name, password }: SignUpInput,
+    @Ctx() { prisma }: Context
+  ): Promise<AuthPayload> {
+    const passwordHash = await hash(password, { type: argon2id });
+    const user = await prisma.user.create({
+      data: { email, name, passwordHash },
+    });
+
+    return { user };
+  }
+}
 
 export const UserResolvers = [
   AggregateUserResolver,
@@ -13,5 +62,6 @@ export const UserResolvers = [
   FindManyUserResolver,
   FindUniqueUserResolver,
   GroupByUserResolver,
+  UserAuthResolver,
   UserRelationsResolver,
 ] as const;
