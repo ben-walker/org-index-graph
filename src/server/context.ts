b73bbb92cf084be4f88ca26266f0b114c6b2881a
Ben Walker <b6nw4r@gmail.com>
2022-03-19 13:29:23 -0400

    Add token TODO

--- src/server/context.ts
+++ src/server/context.ts
@@ -1,4 +1,5 @@
 import { prisma } from "../prisma";
 import { Context } from "./types";
 
+// TODO: add user to context via access token in request
 export const getServerContext = (): Context => ({ prisma });
